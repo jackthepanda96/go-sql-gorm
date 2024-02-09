41d48f011a2669ca64e5eea97301ca145333ff16
Jerry Young <jerryyoung2911@gmail.com>
2024-02-09 19:08:54 +0700

    intial commit

---
+++ users/users.go
@@ -0,0 +1,38 @@
+package users
+
+import "gorm.io/gorm"
+
+type Users struct {
+	HP       string
+	Nama     string
+	Password string
+	Alamat   string
+}
+
+func (u *Users) GantiPassword(connection *gorm.DB, newPassword string) (bool, error) {
+	query := connection.Table("users").Where("hp = ?", u.HP).Update("password", newPassword)
+	if err := query.Error; err != nil {
+		return false, err
+	}
+
+	return query.RowsAffected > 0, nil
+}
+
+func Register(connection *gorm.DB, newUser Users) (bool, error) {
+	query := connection.Create(&newUser)
+	if err := query.Error; err != nil {
+		return false, err
+	}
+
+	return query.RowsAffected > 0, nil
+}
+
+func Login(connection *gorm.DB, hp string, password string) (Users, error) {
+	var result Users
+	err := connection.Where("hp = ? AND password = ?", hp, password).First(&result).Error
+	if err != nil {
+		return Users{}, err
+	}
+
+	return result, nil
+}
